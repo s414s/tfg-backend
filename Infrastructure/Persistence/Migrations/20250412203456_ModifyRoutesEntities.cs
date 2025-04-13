34b0832cf67bac63ab7e414f3cf8db0b70247ddf
Alberto Salas <alsalasher@gmail.com>
2025-04-13 17:17:05 +0200

    Add routes migration

---
+++ Infrastructure/Persistence/Migrations/20250412203456_ModifyRoutesEntities.cs
@@ -0,0 +1,215 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyRoutesEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "FreightRoutes");
+
+            migrationBuilder.DropColumn(
+                name: "DueEnd",
+                table: "Freights");
+
+            migrationBuilder.AddColumn<long>(
+                name: "RouteId",
+                table: "Freights",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L);
+
+            migrationBuilder.UpdateData(
+                table: "MessageThreads",
+                keyColumn: "Id",
+                keyValue: 1L,
+                columns: new[] { "Created", "LastModified" },
+                values: new object[] { new DateTimeOffset(new DateTime(2025, 4, 12, 20, 34, 55, 202, DateTimeKind.Unspecified).AddTicks(6278), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 11, 20, 34, 55, 202, DateTimeKind.Unspecified).AddTicks(6288), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "MessageThreads",
+                keyColumn: "Id",
+                keyValue: 2L,
+                columns: new[] { "Created", "LastModified" },
+                values: new object[] { new DateTimeOffset(new DateTime(2025, 4, 12, 18, 34, 55, 202, DateTimeKind.Unspecified).AddTicks(6309), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 11, 20, 34, 55, 202, DateTimeKind.Unspecified).AddTicks(6312), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "MessageThreads",
+                keyColumn: "Id",
+                keyValue: 3L,
+                columns: new[] { "Created", "LastModified" },
+                values: new object[] { new DateTimeOffset(new DateTime(2025, 4, 11, 20, 34, 55, 202, DateTimeKind.Unspecified).AddTicks(6322), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 11, 20, 34, 55, 202, DateTimeKind.Unspecified).AddTicks(6324), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "MessageThreads",
+                keyColumn: "Id",
+                keyValue: 4L,
+                columns: new[] { "Created", "LastModified" },
+                values: new object[] { new DateTimeOffset(new DateTime(2025, 4, 12, 20, 4, 55, 202, DateTimeKind.Unspecified).AddTicks(6335), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 11, 20, 34, 55, 202, DateTimeKind.Unspecified).AddTicks(6338), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Date",
+                value: new DateTime(2025, 4, 12, 19, 49, 55, 201, DateTimeKind.Utc).AddTicks(8524));
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "Date",
+                value: new DateTime(2025, 4, 12, 20, 4, 55, 201, DateTimeKind.Utc).AddTicks(8536));
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "Date",
+                value: new DateTime(2025, 4, 11, 20, 34, 55, 201, DateTimeKind.Utc).AddTicks(8544));
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "Date",
+                value: new DateTime(2025, 4, 11, 20, 44, 55, 201, DateTimeKind.Utc).AddTicks(8555));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Freights_RouteId",
+                table: "Freights",
+                column: "RouteId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Freights_Routes_RouteId",
+                table: "Freights",
+                column: "RouteId",
+                principalTable: "Routes",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Freights_Routes_RouteId",
+                table: "Freights");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Freights_RouteId",
+                table: "Freights");
+
+            migrationBuilder.DropColumn(
+                name: "RouteId",
+                table: "Freights");
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "DueEnd",
+                table: "Freights",
+                type: "timestamp with time zone",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "FreightRoutes",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    FreightId = table.Column<long>(type: "bigint", nullable: false),
+                    RouteId = table.Column<long>(type: "bigint", nullable: false),
+                    Order = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FreightRoutes", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FreightRoutes_Freights_FreightId",
+                        column: x => x.FreightId,
+                        principalTable: "Freights",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_FreightRoutes_Routes_RouteId",
+                        column: x => x.RouteId,
+                        principalTable: "Routes",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.UpdateData(
+                table: "MessageThreads",
+                keyColumn: "Id",
+                keyValue: 1L,
+                columns: new[] { "Created", "LastModified" },
+                values: new object[] { new DateTimeOffset(new DateTime(2025, 4, 11, 19, 1, 17, 871, DateTimeKind.Unspecified).AddTicks(8473), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 10, 19, 1, 17, 871, DateTimeKind.Unspecified).AddTicks(8477), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "MessageThreads",
+                keyColumn: "Id",
+                keyValue: 2L,
+                columns: new[] { "Created", "LastModified" },
+                values: new object[] { new DateTimeOffset(new DateTime(2025, 4, 11, 17, 1, 17, 871, DateTimeKind.Unspecified).AddTicks(8483), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 10, 19, 1, 17, 871, DateTimeKind.Unspecified).AddTicks(8484), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "MessageThreads",
+                keyColumn: "Id",
+                keyValue: 3L,
+                columns: new[] { "Created", "LastModified" },
+                values: new object[] { new DateTimeOffset(new DateTime(2025, 4, 10, 19, 1, 17, 871, DateTimeKind.Unspecified).AddTicks(8536), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 10, 19, 1, 17, 871, DateTimeKind.Unspecified).AddTicks(8537), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "MessageThreads",
+                keyColumn: "Id",
+                keyValue: 4L,
+                columns: new[] { "Created", "LastModified" },
+                values: new object[] { new DateTimeOffset(new DateTime(2025, 4, 11, 18, 31, 17, 871, DateTimeKind.Unspecified).AddTicks(8542), new TimeSpan(0, 0, 0, 0, 0)), new DateTimeOffset(new DateTime(2025, 4, 10, 19, 1, 17, 871, DateTimeKind.Unspecified).AddTicks(8543), new TimeSpan(0, 0, 0, 0, 0)) });
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 1L,
+                column: "Date",
+                value: new DateTime(2025, 4, 11, 18, 16, 17, 871, DateTimeKind.Utc).AddTicks(5302));
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 2L,
+                column: "Date",
+                value: new DateTime(2025, 4, 11, 18, 31, 17, 871, DateTimeKind.Utc).AddTicks(5308));
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 3L,
+                column: "Date",
+                value: new DateTime(2025, 4, 10, 19, 1, 17, 871, DateTimeKind.Utc).AddTicks(5312));
+
+            migrationBuilder.UpdateData(
+                table: "Messages",
+                keyColumn: "Id",
+                keyValue: 4L,
+                column: "Date",
+                value: new DateTime(2025, 4, 10, 19, 11, 17, 871, DateTimeKind.Utc).AddTicks(5317));
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FreightRoutes_FreightId",
+                table: "FreightRoutes",
+                column: "FreightId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FreightRoutes_RouteId",
+                table: "FreightRoutes",
+                column: "RouteId");
+        }
+    }
+}
