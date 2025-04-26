e069943ca2ed13737afea3dc71eb82f7e6b00f09
Alberto Salas <alsalasher@gmail.com>
2025-04-26 20:44:32 +0200

    Add create Thread

--- Application/Handlers/Threads/Commands/CreateThread.cs
+++ Application/Handlers/Threads/Commands/CreateThread.cs
@@ -1,29 +1,43 @@
 ﻿using Domain.Contracts;
 using Domain.Entities;
+using Domain.Exceptions;
 using MediatR;
+using Microsoft.EntityFrameworkCore;
 
 namespace Application.Handlers.Threads.Commands;
 
 public sealed record CreateThreadRequest : IRequest<long>
 {
-    public required long ToId { get; init; }
-    public required string Text { get; init; } = "";
-    public required string Subject { get; init; } = "";
+    public required string ToEmail { get; init; }
+    public required string Text { get; init; }
+    public required string Subject { get; init; }
 }
 
 internal sealed class CreateThreadCommandHandler : IRequestHandler<CreateThreadRequest, long>
 {
     private readonly IRepository<MessageThread> _messageThreadRepository;
+    private readonly IRepository<User> _usersRepository;
     private readonly IUserInfo _activeUserInfo;
 
-    public CreateThreadCommandHandler(IUserInfo activeUserInfo, IRepository<MessageThread> messageThreadRepository)
+    public CreateThreadCommandHandler(
+        IUserInfo activeUserInfo,
+        IRepository<MessageThread> messageThreadRepository,
+        IRepository<User> usersRepository)
     {
         _activeUserInfo = activeUserInfo;
         _messageThreadRepository = messageThreadRepository;
+        _usersRepository = usersRepository;
     }
 
     public async Task<long> Handle(CreateThreadRequest request, CancellationToken cancellationToken)
     {
+        var toUser = await _usersRepository.Query
+            .FirstOrDefaultAsync(x => x.Email == request.ToEmail.ToLower(), cancellationToken)
+            ?? throw new EntityNotFoundException(nameof(User));
+
+        //if (_activeUserInfo.User.Id == toUser.Id)
+        //    throw new CustomException("You can not send a message to your self");
+
         var newMessage = new Message
         {
             Text = request.Text,
@@ -32,7 +46,7 @@ internal sealed class CreateThreadCommandHandler : IRequestHandler<CreateThreadR
 
         var newThread = MessageThread.Create(
             author: _activeUserInfo.User.Id,
-            to: request.ToId,
+            to: toUser.Id,
             subject: request.Subject,
             message: newMessage
             );
